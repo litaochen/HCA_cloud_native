a03134c3a31ea354ed86c3eba682aa53a04b56db
Litao Chen <litaochen@g.harvard.edu>
2019-06-22 23:49:38 -0400

    job dispatcher worked.

--- job_dispatcher/job_dispatcher.py
+++ job_dispatcher/job_dispatcher.py
@@ -1,45 +1,66 @@
-import csv
-import xml.etree.ElementTree as ET
-import sys
-import os
-import json
 import boto3
+import json
+import os
+import sys
+import xml.etree.ElementTree as ET
+import csv
 
-from .data_selector import find_by_extension
+import s3worker
 
-# constants for dev
-# will be converted into arguments later
-queue_url = 'https://sqs.us-east-1.amazonaws.com/263120685370/HCA-tasks'
-input = {
-    'image_data': {
-        's3_bucket': 'hca-cloud-native', 
-        'prefix': 'example_data/'
-    },
-    'pipeline_file': {
-        's3_bucket': 'hca-cloud-native', 
-        'key': 'pipeline_files/test.cppipe' 
-    }
-}
 
-# the location to store the analysis result
-# sub-dir structure will be created to save result from each taks
-output_directory = {
-    's3_bucket': 'hca-cloud-native',
-    'prefix': 'analysis_result/'
-}
+# parse and submit job request
+# args:
+#   - job_id: the unique job id for current job request
+#   - input: a dict describes the input dir in s3 bucket, pipeline file
+#   - metadata_file_extension: the extention of  the metadata file: "xdce", no dot.
+#   - job_record_dir: a dict describes the output dir in s3 bucket
+#                  certain dir structure will be created to hold output from each job
 
-# the job id. It is part of the output directory structure
-job_id = "20190621_ABC123"
+
+def submit_job(job_id, input, metadata_file_extention, job_record_dir):
+    s3 = boto3.client('s3')
+
+    the_bucket = input['image_data']['s3_bucket']
+    the_prefix = input['image_data']['prefix']
+
+    # check if output_path has "/" at the end, if not, add one
+    if job_record_dir[-1:] != '/':
+        the_valid_job_record_dir = job_record_dir + '/'
+    else:
+        the_valid_job_record_dir = job_record_dir
+
+    input_file_prefix = the_valid_job_record_dir + job_id + "/input/"
+
+# find the metadata file first
+    metadata_files = s3worker.find_by_extension(
+        s3, the_bucket, the_prefix, metadata_file_extention)
+    try:
+        assert len(metadata_files) == 1
+    except AssertionError as e:
+        e.args += ("Found " + str(len(metadata_files)) +
+                   " metadata file(s) with extension: ", 'xdce')
+        raise e
+
+    metadata_file = metadata_files[0]
+#  download the metadata file to /tmp (will be more efficient when the file is big)
+# /tmp is guaranteed to be available during the execution of your Lambda function
+    local_copy = s3worker.download_file(
+        s3, the_bucket, metadata_file, '/tmp')
+
+    rows = parse_metadata_file(local_copy)
+
+# save individual image file list by well and add to queue
+    create_tasks(s3, the_bucket, job_id, rows, input_file_prefix)
+
+    count_tasks_in_queue()
 
 
 # parse the metadata and return a list of dictionaries ready to save as csv
 # args:
 #   - path_to_xml_doc: the path to the xdce file
 #   - returns: a list of dictionaries ready to save as csv
-
-
-def parse_metadata_file(path_to_xml_doc):
-    tree = ET.parse(path_to_xml_doc)
+def parse_metadata_file(path_metadata_file):
+    tree = ET.parse(path_metadata_file)
     root = tree.getroot()
     groups = {}
 
@@ -62,15 +83,72 @@ def parse_metadata_file(path_to_xml_doc):
                 "Column_Number": column,
                 "Well_Location": well,
                 "Field_Index": field,
-                URL_title: "file:" + path_to_xml_doc + filename
+                URL_title: "file:" + path_metadata_file + filename
             }
         else:
-            groups[group_id][URL_title] = "file:" + path_to_xml_doc + filename
+            groups[group_id][URL_title] = "file:" + \
+                path_metadata_file + filename
 
     rows = []
     for key, val in groups.items():
         rows.append(val)
     return rows
+
+
+# write rows to csv file and submit to sqs, each file contains groups from the same well
+# args:
+#   -s3_client: the s3 client used to interact with S3 bucket
+#   -the_bucket: the S3 bucket to save the file list
+#   -job_id: the job id. Will be written in the message to worker.
+#   - rows: a list of dict, each dict is one row of the image list,
+#         contains images from the same well, same field
+#   - input_file_prefix: the path to save the csv files as task input
+#   - each task will have individual directory for the image list and output
+#   - a post-processing lambda will combine the output to sigle result file later
+
+# todo: use boto3 to save the file to S3 (local -> S3)
+def create_tasks(s3_client, the_bucket, the_job_id, rows, input_file_prefix):
+    # the input_file_prefix should have "/" at the end
+    # if not, add it
+    if input_file_prefix[-1:] != '/':
+        the_valid_input_file_prefix = input_file_prefix + '/'
+    else:
+        the_valid_input_file_prefix = input_file_prefix
+
+    current_well = ""
+    rows_for_task = []
+
+    # add a dummy row to flush the last well
+    rows.append({"Well_Location": "dummy"})
+    for row in rows:
+        if row['Well_Location'] != current_well:
+            # one well is done. save to local file under "/tmp" and upload to S3
+            if current_well != "":
+                local_file = '/tmp/' + the_job_id + '-' + current_well + '.csv'
+                file_list_key = the_valid_input_file_prefix + \
+                    current_well + '/' + current_well + ".csv"
+
+                save_to_csv(rows_for_task, local_file)
+                s3worker.upload_file(s3_client, local_file,
+                                     the_bucket, file_list_key)
+                os.remove(local_file)
+                rows_for_task = []
+
+                # enqueue the task
+                message = json.dumps({
+                    "job_id": the_job_id,
+                    "task_id": current_well,
+                    "file_list": {
+                        "bucket": the_bucket,
+                        "file_list_key": file_list_key
+                    },
+                    "pipeline_file": "path_to_pipeline_file",
+                    "output_dir": input_file_prefix
+                })
+                enqueue_task(message)
+
+            current_well = row['Well_Location']
+        rows_for_task.append(row)
 
 
 # helper function to save dict to csv file
@@ -116,117 +194,35 @@ def count_tasks_in_queue():
     print(response)
 
 
-# write rows to csv file and submit to sqs, each file contains groups from the same well
-# args:
-#   - rows: a list of dict, each dict is one row of the image list,
-#         contains images from the same well, same field
-#   - output_path: the path to save the csv files
-#   - each task will have individual directory for the image list and output
-#   - a post-processing lambda will combine the output to sigle result file later
-
-# todo: use boto3 to save the file to S3 (local -> S3)
-def create_tasks(rows, output_path):
-    print("splitting job into tasks:")
-    current_well = ""
-    rows_for_task = []
-
-    # add a dummy row to flush the last well
-    rows.append({"Well_Location": "dummy"})
-    for row in rows:
-        if row['Well_Location'] != current_well:
-            if current_well != "":
-                file_list = output_path + '/' + current_well + ".csv"
-                save_to_csv(rows_for_task, file_list)
-                rows_for_task = []
-
-                # enqueue the task
-                message = json.dumps({
-                    "job_id": "ABC123",
-                    "task_id": current_well,
-                    "file_list": file_list,
-                    "pipeline_file": "path_to_pipeline_file",
-                    "output_dir": output_path
-                })
-                enqueue_task(message)
-
-            current_well = row['Well_Location']
-        rows_for_task.append(row)
-
-
-# helper function to download file to local disk
-# here is mainly used to download the metadata file
-#   args:
-#   - the_bucket: the s3 bucket where the file is stored
-#   - the_key:   the key of the file to download
-#   - destination:  the location to save the file. For Lambda, it is /tmp
-#   - it will return the path to the file on local disk
-def download_from_s3(the_bucket, the_key, destination):
-    s3 = boto3.client('s3')
-
-    filename = the_key.split('/')[-1]
-    saved_as = destination + filename
-
-    with open(saved_as, 'wb') as data:
-        s3.download_fileobj(the_bucket, the_key, data)
-
-    return saved_as
-
-# parse and submit job request
-# args:
-#   - input: a dict describes the input dir in s3 bucket, pipeline file
-#   - metadata_file_extension: the extention of  the metadata file: "xdce", no dot.
-#   - output_path: a dict describes the output dir in s3 bucket
-#                  certain dir structure will be created to hold output from each job
-def submit_job(input, metadata_file_extention, output_path):
-    s3 = boto3.client('s3')
-
-    the_bucket = input['image_data']['s3_bucket']
-    the_prefix = input['image_data']['prefix']
-    
-
-
-# find the metadata file first
-    metadata_file_key = find_by_extension(the_bucket, the_prefix, metadata_file_extention)
-
-#  download the metadata file to /tmp (will be more efficient when the file is big)
-# /tmp is guaranteed to be available during the execution of your Lambda function
-    local_copy = download_from_s3(the_bucket, metadata_file_key, "/tmp")
-    rows = parse_metadata_file(local_copy)
-
-# save individual image file list by well and add to queue
-    create_tasks(rows, output_path + job_id + "/")
-
-    count_tasks_in_queue()
-
-
-# entry point of the script
-if __name__ == '__main__':
-    # only accept one file
-    if len(sys.argv) != 3:
-        print("wrong number of arguments.")
-        print("usage: python metadata_parser.py xdce_file output_dir")
-        exit(1)
-
-    # check if file exists
-    if not os.path.isfile(sys.argv[1]):
-        print("file does not exist: " + sys.argv[1])
-        exit(1)
-
-    # check if output dir exists
-    if not os.path.isdir(sys.argv[2]):
-        print("output directory does not exist: " + sys.argv[2])
-        exit(1)
-
-    # set common variables
-    xml_doc = sys.argv[1]
-    output_path = sys.argv[2]
-
-    submit_job(xml_doc, output_path)
-
-
 # test the procrdure:
 #   - read metadata from input dir ".xdce file"
 #   - write file list to to the output dir
 #   - submit taks to sqs
 #   - check number of tasks in sqs
 
+# constants for dev
+# will be converted into arguments later
+queue_url = 'https://sqs.us-east-1.amazonaws.com/263120685370/HCA-tasks'
+input = {
+    'image_data': {
+        's3_bucket': 'hca-cloud-native',
+        'prefix': 'example_data/'
+    },
+    'pipeline_file': {
+        's3_bucket': 'hca-cloud-native',
+        'key': 'pipeline_files/test.cppipe'
+    }
+}
+
+# the location to store the analysis result
+# sub-dir structure will be created to save result from each taks
+output_directory = {
+    's3_bucket': 'hca-cloud-native',
+    'prefix': 'analysis_result/'
+}
+
+# the job id. It is part of the output directory structure
+job_id = "20190621_ABC123"
+
+
+submit_job(job_id, input, 'xdce', 'analysis_result/')
---
+++ job_dispatcher/s3worker.py
@@ -0,0 +1,134 @@
+# this file contains some useful functions to operate on S3 bucket
+# resuable for other part of our app
+
+import boto3
+
+# get list if directories / flies in current "directory"
+# args:
+#   - s3_client: a boto3 s3 client to work on S3 bucket
+#   - the_bucket: the bucket to work on
+#   - the_prefix: the prefix to the target directory. Must have "/" at the end
+#   - num_items: number of item to retrieve, by default no limit.
+
+
+def get_content_in_dir(s3_client, the_bucket, the_prefix, max_items=None):
+    # check if prefix has "/" at the end, if not, add one
+    if the_prefix[-1:] != '/':
+        the_valid_prefix = the_prefix + '/'
+    else:
+        the_valid_prefix = the_prefix
+
+    kwargs = {'Bucket': the_bucket,
+              'Prefix': the_valid_prefix,
+              'Delimiter': '/',
+              'StartAfter': the_valid_prefix
+              }
+
+    if not max_items is None:
+        kwargs['MaxKeys'] = max_items
+
+    # get dir or file list
+    content = {'dirs': [], 'files': []}
+    while True:
+        response = s3_client.list_objects_v2(**kwargs)
+
+        if 'CommonPrefixes' in response.keys():
+            for item in response['CommonPrefixes']:
+                content['dirs'].append(item['Prefix'])
+
+        if 'Contents' in response.keys():
+            for item in response['Contents']:
+                content['files'].append(item['Key'])
+
+        try:
+            kwargs['ContinuationToken'] = response['NextContinuationToken']
+        except KeyError:
+            break
+
+    return content
+
+
+# check if certain file type exists in current dir
+# mainly used to check if the metadata file "xdce" exists.
+# if find the file, return a list of the keys of the files
+# args:
+#   - s3_client: a boto3 s3 client to work on S3 bucket
+#   - the_bucket: the bucket to work on
+#   - the_prefix: the prefix to the target directory. Must have "/" at the end
+def find_by_extension(s3_client, the_bucket, the_prefix, file_extension):
+    # check if prefix has "/" at the end, if not, add one
+    if the_prefix[-1:] != '/':
+        the_valid_prefix = the_prefix + '/'
+    else:
+        the_valid_prefix = the_prefix
+
+    content = get_content_in_dir(s3_client, the_bucket, the_valid_prefix)
+
+    target_files = []
+
+    for item in content['files']:
+        if item.split('.')[-1] == file_extension:
+            target_files.append(item)
+
+    return target_files
+
+
+# download file from s3 to local storge
+# mainly used to download input files for CellProfiler to use, file-list, pipeline file
+#   args:
+#   - s3_client: a boto3 s3 client to work on S3 bucket
+#   - the_bucket: the s3 bucket where the file is stored
+#   - the_key:   the key of the file to download
+#   - destination:  the location to save the file. For Lambda, it is /tmp
+#   - it will return the path to the file on local disk
+
+def download_file(s3_client, the_bucket, the_key, destination):
+    filename = the_key.split('/')[-1]
+    saved_as = destination + '/' + filename
+
+    with open(saved_as, 'wb') as data:
+        s3_client.download_fileobj(the_bucket, the_key, data)
+
+    return saved_as
+
+
+# upload file from s3 to local storge
+#   args:
+#   - s3_client: a boto3 s3 client to work on S3 bucket
+#   - the_bucket: the s3 bucket where the file is stored
+#   - the_key:   the key of the file, decide where the file goes in the bucket
+#   - it will return the path to the file on local disk
+
+def upload_file(s3_client, path_to_file, the_bucket, the_key):
+    try:
+        with open(path_to_file, 'rb') as data:
+            s3_client.upload_fileobj(data, the_bucket, the_key)
+    except IOError as e:
+        e.args += ("Can't read file: ", path_to_file)
+        raise e
+
+
+# # quick test
+# bucket = 'hca-cloud-native'
+# prefix = 'example_data/'
+# key_of_file = 'example_data/example.xdce'
+# test_dir = 'test/'
+
+# s3 = boto3.client('s3')
+
+# # list dir content
+# content = get_content_in_dir(s3, bucket, prefix)
+# print(content['dirs'])
+# print(len(content['files']))
+
+# # get metadata
+# metadata_files = find_by_extension(s3, bucket, prefix, 'xdce')
+# print(metadata_files)
+
+# # download file test
+# local_copy_location = download_file(s3, bucket, key_of_file, '/tmp')
+# print("location of the local copy: " + local_copy_location)
+
+
+# # upload a file test
+# upload_file(s3, '/tmp/example.xdce', bucket, test_dir + "test_upload.xdce")
