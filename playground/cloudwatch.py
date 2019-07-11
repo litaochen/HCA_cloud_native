6be052916d7eec5f2e23e91cf2b301411f978aad
Chen, Eric-2 <eric-2.chen@novartis.com>
2019-07-11 15:17:12 -0400

    watchtower setup

--- playground/cloudwatch.py
+++ playground/cloudwatch.py
@@ -8,10 +8,19 @@ import watchtower, logging
 LOG_GROUP_NAME = 'hca-cloud-native'
 LOG_STREAM_NAME = 'cellprofiler'
 
+watchtower_config = {
+    'log_group': LOG_GROUP_NAME,
+    'stream_name': LOG_STREAM_NAME,
+    'use_queues': True,
+    'create_log_group': False
+}
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
 
-watchtowerlogger = watchtower.CloudWatchLogHandler(log_group=LOG_GROUP_NAME, stream_name=LOG_STREAM_NAME, create_log_group=False)
+watchtowerlogger = watchtower.CloudWatchLogHandler(**watchtower_config)
+watchtowerlogger.setFormatter(formatter)
 logger.addHandler(watchtowerlogger)
 
 logger.info("Hi")
